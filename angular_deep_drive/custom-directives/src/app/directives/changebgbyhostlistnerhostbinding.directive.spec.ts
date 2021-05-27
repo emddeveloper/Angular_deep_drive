e93a761a15ef51060333f64d9c7258cff43ab051
emddeveloper <emd.developer@gmail.com>
2021-05-27 21:51:34 +0530

    custom directives using elementRef renderer2 hostlistner hostbinding etc

---
+++ angular_deep_drive/custom-directives/src/app/directives/changebgbyhostlistnerhostbinding.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ChangebgbyhostlistnerhostbindingDirective } from './changebgbyhostlistnerhostbinding.directive';
+
+describe('ChangebgbyhostlistnerhostbindingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ChangebgbyhostlistnerhostbindingDirective();
+    expect(directive).toBeTruthy();
+  });
+});
